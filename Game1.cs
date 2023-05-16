79524da5c55924fd861c149649aa4c4c1b321750
Mehrbod Mehrabi <shamila.gaff@gmail.com>
2023-05-15 20:22:14 -0400

    adding a custom cursor

--- Game1.cs
+++ Game1.cs
@@ -11,19 +11,23 @@ namespace SFX
 
 
         private Sprite _mario;
+        private bool _mouseVisible;
+        private Sprite _mouse;
+        private MouseState _mouseState;
        
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
             
+
             base.Initialize();
         }
 
@@ -33,7 +37,7 @@ namespace SFX
 
             // TODO: use this.Content to load your game content here
             var texture1 = Content.Load<Texture2D>("mario");
-
+            //var mousetexture = Content.Load<Texture2D>("mouse");
 
             _mario = new Sprite(texture1)
             {
@@ -49,15 +53,33 @@ namespace SFX
                 }
             };
 
-            
+            _mouse = new Sprite(texture1)
+            {
+                _position = new Vector2(),
+                _speed = 5f,
+                _scaling = new Vector2(0.1f, 0.1f),
+                _input = new Input()
+                {
+                    Up = Keys.W,
+                    Down = Keys.S,
+                    Left = Keys.A,
+                    Right = Keys.D,
+                }
+            };
+
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-        
+
+            _mouseState = Mouse.GetState();
             _mario.Update();
+            //_mousePosition = Vector2(_mouseState.X, _mouseState.Y);
+            _mouse._position.X = _mouseState.X;
+            _mouse._position.Y = _mouseState.Y;
 
             // TODO: Add your update logic here
 
@@ -72,6 +94,7 @@ namespace SFX
             _spriteBatch.Begin();
            
             _mario.Draw(_spriteBatch); 
+            _mouse.Draw(_spriteBatch);
            
             _spriteBatch.End();
 
