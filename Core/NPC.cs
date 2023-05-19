7a6c6b7d7fd9fb72037e0021204914b775eee8bc
Mehrbod Mehrabi <shamila.gaff@gmail.com>
2023-05-19 00:30:13 -0400

    Some cleaning and stuff

---
+++ Core/NPC.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFX.Core
+{
+    internal class NPC
+    {
+        public string name;
+        private Texture2D _texture;
+        public Vector2 _position;
+        public Vector2 _scaling;
+        public NPC(Texture2D _texture)
+        {
+            this._texture = _texture;
+        }
+
+        public void playerName(string name)
+        {
+            this.name = name;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, null, Color.White, 0, Vector2.Zero, _scaling, SpriteEffects.None, 0);
+
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -11,16 +11,14 @@ namespace SFX
 
 
         private Sprite _mario;
-        private bool _mouseVisible;
-        private Sprite _mouse;
-        private MouseState _mouseState;
+
        
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = false;
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
@@ -36,28 +34,13 @@ namespace SFX
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            var texture1 = Content.Load<Texture2D>("mario");
-            //var mousetexture = Content.Load<Texture2D>("mouse");
-
+            var texture1 = Content.Load<Texture2D>("shepard");
+           
             _mario = new Sprite(texture1)
             {
                 _position = new Vector2(70, 70),
                 _speed = 5f,
-                _scaling = new Vector2(0.1f, 0.1f),
-                _input = new Input()
-                {
-                    Up = Keys.W,
-                    Down = Keys.S,
-                    Left = Keys.A,
-                    Right = Keys.D,
-                }
-            };
-
-            _mouse = new Sprite(texture1)
-            {
-                _position = new Vector2(),
-                _speed = 5f,
-                _scaling = new Vector2(0.1f, 0.1f),
+                _scaling = new Vector2(2f, 2f),
                 _input = new Input()
                 {
                     Up = Keys.W,
@@ -75,13 +58,9 @@ namespace SFX
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            _mouseState = Mouse.GetState();
-            _mario.Update();
-            //_mousePosition = Vector2(_mouseState.X, _mouseState.Y);
-            _mouse._position.X = _mouseState.X;
-            _mouse._position.Y = _mouseState.Y;
-
             // TODO: Add your update logic here
+            _mario.Update();
+
 
             base.Update(gameTime);
         }
@@ -94,7 +73,7 @@ namespace SFX
             _spriteBatch.Begin();
            
             _mario.Draw(_spriteBatch); 
-            _mouse.Draw(_spriteBatch);
+           
            
             _spriteBatch.End();
 
